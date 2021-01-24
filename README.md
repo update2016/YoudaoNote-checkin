f2a1e890c0dbdcb9e6ac83122020671ae7aaeb6d
QikaiXu <691924095@qq.com>
2021-01-24 14:07:38 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# YoudaoNote-checkin
-有道云笔记，通过 github action 每天自动签到获得存储空间。
+# 有道云笔记签到
+
+一点小羊毛
+
+通过 github action 来实现自动签到（每天签到可以获得存储空间奖励）
+
+步骤：
+
+1. 设置用户名（USERNAME）和密码（PASSWORD）
+
+   先 fork 这个仓库，在仓库中依次点击 `Settings` - `Secrets` - `New repository secret`，如下图所示
+
+   ![image-20210111220035535](README.assets/image-20210111220035535.png)
+
+   然后添加 2 个 secret，分别为 `USERNAME` 和 `PASSWORD`。
+
+   `USERNAME` 就是签到账号，示例：
+
+   ```text
+   150********@163.com
+   ```
+
+   `PASSWORD` 是密码的 32 位 MD5 值，可以自己去[有道云笔记](https://note.youdao.com/signIn/index.html)网页登录一下获得，也可以去各种网站计算 MD5 值，示例：
+
+   ```text
+   e10adc3949ba59abbe56e057f20f883e
+   ```
+
+3. 随便发起一个 push 请求，可以修改一下 `README.md`，或者自己给自己点个 star，就可以开始。之后就会每小时进行一次签到（因为有时候签到会失败，好像是服务器不太好，就设置一下每小时签到一次保证成功吧）。
+
+注意：在官方文档中有这么一段
+
+> To prevent unnecessary workflow runs, scheduled workflows may be disabled automatically. When a public repository is forked, scheduled workflows are disabled by default. In a public repository, scheduled workflows are automatically disabled when no repository activity has occurred in 60 days.
+
+也就是说，**定时执行的任务需要每隔 60 天激活一次**。
+
---
+++ checkin.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import time
+import urllib3
+import os
+
+urllib3.disable_warnings()
+
+
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+
+
+def checkin(username: str, password: str):
+    login_url = 'https://note.youdao.com/login/acc/urs/verify/check?app=web&product=YNOTE&tp=urstoken&cf=6&fr=1&systemName=&deviceType=&ru=https%3A%2F%2Fnote.youdao.com%2FsignIn%2F%2FloginCallback.html&er=https%3A%2F%2Fnote.youdao.com%2FsignIn%2F%2FloginCallback.html&vcode=&systemName=mac&deviceType=MacPC&timestamp=1611466345699'
+    checkin_url = 'http://note.youdao.com/yws/mapi/user?method=checkin'
+    parame = {
+        'username': username,
+        'password': password
+    }
+
+    s = requests.Session()
+    # 登录
+    s.post(url=login_url, data=parame, verify=False)
+
+    # 签到
+    r = s.post(url=checkin_url)
+    print(r.text)
+    if r.status_code == 200:
+        info = json.loads(r.text)
+        total = info['total'] / 1048576
+        space = info['space'] / 1048576
+        t = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(info['time'] / 1000))
+
+        print('{} | 这次签到获得：{} M | 总共获得：{} M | 签到时间：{}'.format(username, space, total, t))
+
+
+if __name__ == "__main__":
+    checkin(username, password)
