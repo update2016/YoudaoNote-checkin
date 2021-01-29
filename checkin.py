2e3a0b8d42a733857b8b4a5c2d5f2b53a5f10ed2
QikaiXu <691924095@qq.com>
2021-01-29 13:30:11 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -16,12 +16,9 @@ def checkin(username: str, password: str):
     login_url = 'https://note.youdao.com/login/acc/urs/verify/check?app=web&product=YNOTE&tp=urstoken&cf=6&fr=1&systemName=&deviceType=&ru=https%3A%2F%2Fnote.youdao.com%2FsignIn%2F%2FloginCallback.html&er=https%3A%2F%2Fnote.youdao.com%2FsignIn%2F%2FloginCallback.html&vcode=&systemName=mac&deviceType=MacPC&timestamp=1611466345699'
     checkin_url = 'http://note.youdao.com/yws/mapi/user?method=checkin'
 
-    m = hashlib.md5()
-    m.update(password.encode('utf8'))
-
     parame = {
         'username': username,
-        'password': m.hexdigest(),
+        'password': hashlib.md5(password.encode('utf8')).hexdigest(),
     }
 
     s = requests.Session()
